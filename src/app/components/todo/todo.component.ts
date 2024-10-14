f001674b072a394efbadfb7f3efa63885574637f
JuanManuelGomezVieira <92581383+JuanManuelGomezVieira@users.noreply.github.com>
2024-10-14 18:22:33 +0200

    finished first version of todo App with Angular, working with localStorage right now

--- src/app/components/todo/todo.component.ts
+++ src/app/components/todo/todo.component.ts
@@ -1,4 +1,4 @@
-import { Component, signal } from '@angular/core';
+import { Component, OnInit, computed, effect, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FilterType, TodoModel } from '../../models/todo';
 import { FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
@@ -11,34 +11,44 @@ import { FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
   styleUrl: './todo.component.css'
 })
 
-export class TodoComponent {
+export class TodoComponent implements OnInit {
   todolist = signal<TodoModel[]>([
-    {
-      id: 1,
-      title: 'Task 1',
-      completed: false,
-      editing: false,
-    },
-    {
-      id: 2,
-      title: 'Task 2',
-      completed: false,
-      editing: false,
-    },
-    {
-      id: 3,
-      title: 'Task 3',
-      completed: false,
-      editing: false,
-    },
+
   ]);
 
   filter = signal<FilterType>('all');
+
+  todoListFiltered = computed(() => {
+    const filter = this.filter()
+    const todos = this.todolist()
+
+    switch (filter) {
+      case 'active':
+        return todos.filter((todo) => !todo.completed)
+      case 'completed':
+        return todos.filter((todo) => todo.completed)
+      default:
+        return todos
+    }
+  })
 
   newTodo = new FormControl('', {
     nonNullable: true,
     validators: [Validators.required, Validators.minLength(3)]
   })
+
+  constructor() {
+    effect(() => {
+      localStorage.setItem('todos', JSON.stringify(this.todolist()))
+    })
+  }
+
+  ngOnInit(): void{
+    const storage = localStorage.getItem('todos')
+    if (storage) {
+      this.todolist.set(JSON.parse(storage))
+    }
+  }
 
   changeFilter(filterString: FilterType) {
     this.filter.set(filterString);
@@ -58,5 +68,40 @@ export class TodoComponent {
     } else {
       this.newTodo.reset()
     }
+  }
+
+  toggleTodo(todoId: number) {
+    return this.todolist.update((prev_todos) =>
+      prev_todos.map((todo) => {
+
+        return todo.id === todoId ?
+          {...todo, completed: !todo.completed}
+          : todo
+
+      })
+    )
+  }
+
+  removeTodo(todoId: number){
+    this.todolist.update((prev_todos) => prev_todos.filter((todo) => todo.id !== todoId))
+  }
+
+  updateTodoEditingMode(todoId: number){
+    return this.todolist.update((prev_todos) =>
+      prev_todos.map((todo) => {
+        return todo.id === todoId ?
+          {...todo, editing: true}
+          : {...todo, editing: false}
+      })
+    )
+  }
+
+  saveTitleTodo(todoId: number, event: Event){
+    const title = (event.target as HTMLInputElement).value;
+    return this.todolist.update((prev_todos) => prev_todos.map((todo) => {
+      return todo.id === todoId ?
+        {...todo, title: title, editing: false}
+        : todo
+    }))
   }
 }
